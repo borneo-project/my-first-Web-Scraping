c566ada276b6feafb467ae7993cc82583ecc9fa0
erwanlab <erwan.mspg@gmail.com>
2024-11-13 13:58:06 +0800

    update import

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 import requests
 import pandas
-import pandas as pd
-import seaborn as snn
-import matplotlib.pyplot as plt
-from bs4 import BeautifulSoup as bs4
+import seaborn
+import matplotlib.pyplot
+from bs4 import BeautifulSoup
 
 continents_page = requests.get("https://simple.wikipedia.org/wiki/List_of_countries_by_continents").text
-print(continents_page)
+continents_countries_soup = BeautifulSoup(continents_page, 'html.parser')
+continents = continents_countries_soup.find_all('h2' > 'span', {"class":"mw-heading mw-heading2"})
+unwanted_word = ["Antartica","Notes","References","Other website"]
+target_continents = [continents.text for continents in continents if continents.text not in unwanted_word]
+print(continents)
+
+

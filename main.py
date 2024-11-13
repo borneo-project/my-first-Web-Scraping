f8d827f225b9e65cddf0d914bd4ad1400193936f
erwanlab <erwan.mspg@gmail.com>
2024-11-13 14:15:57 +0800

    update import

--- main.py
+++ main.py
@@ -7,8 +7,8 @@ from bs4 import BeautifulSoup
 continents_page = requests.get("https://simple.wikipedia.org/wiki/List_of_countries_by_continents").text
 continents_countries_soup = BeautifulSoup(continents_page, 'html.parser')
 continents = continents_countries_soup.find_all('h2' > 'span', {"class":"mw-heading mw-heading2"})
-unwanted_word = ["Antartica","Notes","References","Other website"]
-target_continents = [continents.text for continents in continents if continents.text not in unwanted_word]
+unwanted_word = ("Antarctica","Notes","References","Other website")
+target_continents = (continents.text for continents in continents if continents.text not in unwanted_word)
 print(continents)
 
 

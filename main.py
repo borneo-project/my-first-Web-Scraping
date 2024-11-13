f88ce2b4cbdc0e3c3ca6e4d9c5db8d421ed9b912
erwanlab <erwan.mspg@gmail.com>
2024-11-13 12:28:45 +0800

    update import

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
 import requests
 import pandas as pd
-import seaborn as sns
+import seaborn as snn
 import matplotlib.pyplot as plt
-from bs4 import BeautifulSoup as bs4
+from bs4 import BeautifulSoup
+
+continents_page = requests.get("https://simple.wikipedia.org/wiki/List_of_countries_by_continents").text
+continents_page
+
+continents_countries_soup = bs4(continents_page,"html")
+continents = continents_countries_soup.find_all('h2' > 'span', {"class":"mw-heading mw-heading2"})
+continents
